36175cea5f583c481d4058c67623998ee49aacaf
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2022-10-19 19:46:41 -0400

    Primeiro Commit

---
+++ src/introducaogit/IntroducaoGIT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package introducaogit;
+
+/**
+ *
+ * @author clebe
+ */
+public class IntroducaoGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nosso primeiro projeto com gerenciamento de Versão");
+    }
+    
+}
